37851d49c0cad56da6b9b7401f767642e7d79c30
Vladimir Evdokimov <vladimir@vladimirevdokimov.ru>
2024-03-26 00:59:39 +0500

    Misspelling fix Task4 and comments Task5

--- Task5/adams.m
+++ Task5/adams.m
@@ -11,6 +11,7 @@ function y = adams(f, x0, y0, x, n)
     y = y0;
     f_n = [y0 0 0 0];
 
+    % initial values by Runge-Kutta
     for i = 1:3
         k0 = f(x0,         y);
         k1 = f(x0 + h / 2, y + h * k0 / 2);
@@ -23,11 +24,13 @@ function y = adams(f, x0, y0, x, n)
     end
     
     for i = 4:n
+        % predictor
         yy = y + h / 24 * (55 * f_n(4) - 59 * f_n(3) + ...
                           37 * f_n(2) - 9 * f_n(1));
         x0 = x0 + h;
         f_n = [f_n(2:4) f(x0, yy)];
-    
+        
+        % corrector
         y = y + h / 24 * (9 * f_n(4) + 19 * f_n(3) - ...
                           5 * f_n(2) + f_n(1));
         f_n(4) = f(x0, y);
